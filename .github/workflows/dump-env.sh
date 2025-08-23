d6f0284ce1b0e953b993402de3375bcb915a963d
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2025-08-23 17:23:49 -0400

    playing with GH workflows #3

---
+++ .github/workflows/dump-env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "... starting the script $0 in $(pwd) ..."
+echo "--------- env-vars before executing the maven: -----------
+env | sort
+echo "----------------------------------------------------------
