cead81f70726b9e8d202196fbd7d20706dc30752
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2025-08-24 16:38:21 -0400

    Play with java and maven #0

---
+++ src/main/java/org/krmdemo/techlabs/stream/TechlabsStreamUtils.java
@@ -0,0 +1,9 @@
+package org.krmdemo.techlabs.stream;
+
+public class TechlabsStreamUtils {
+
+    private TechlabsStreamUtils() {
+        // prohibit the creation of utility-class instance
+        throw new UnsupportedOperationException("Cannot instantiate utility-class " + getClass().getName());
+    }
+}
---
+++ src/test/java/org/krmdemo/techlabs/thymeleaf/ThymeleafTemplateTest.java
@@ -0,0 +1,11 @@
+package org.krmdemo.techlabs.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+
+public class ThymeleafTemplateTest {
+
+    @Test
+    void testEnvVars() {
+
+    }
+}
