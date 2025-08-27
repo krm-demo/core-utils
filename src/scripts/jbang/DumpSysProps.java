c7a10206bb7494fd109b0a89f7caba4a6d8e7f84
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2025-08-26 20:18:43 -0400

    improve the result of 'dump-to-summary.sh' #3

--- src/scripts/jbang/DumpSysProps.java
+++ src/scripts/jbang/DumpSysProps.java
@@ -1,13 +1,13 @@
 ///usr/bin/env jbang  --quiet "$0" "$@" ; exit $?
 //SOURCES JBangUtils.java
 
-public class DumpEnvVars {
+public class DumpSysProps {
 
     /**
      * JBang and JVM entry-point
      * @param args command-line arguments that are unused in this JBang-script
      */
     public static void main(String... args) {
-        System.out.println(JBangUtils.dumpEnvVarsExAsJson());
+        System.out.println(JBangUtils.dumpSysPropsAsJson());
     }
 }
