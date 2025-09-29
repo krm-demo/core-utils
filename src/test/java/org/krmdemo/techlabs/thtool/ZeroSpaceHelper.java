9c2e9268c1c6a28f1df0d5ddc00ffb433196b5d7
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2025-09-29 13:41:06 -0400

    Introduce ZeroSpaceHelper

---
+++ src/test/java/org/krmdemo/techlabs/thtool/ZeroSpaceHelper.java
@@ -0,0 +1,91 @@
+package org.krmdemo.techlabs.thtool;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * A helper-class to obfuscate the strings with zero-spaces.
+ * <hr/>
+ * The methods of this helper-class allows to inject {@code &ZeroWidthSpace;} or {@code &#8203;}
+ * or {@code &#x200B;} in the middle of passed sensitive string. In most cases it's enough
+ * the result string to be allowed to display or even to commit.
+ * <hr/>
+ * In order to partially violate the security rule, which prohibits displaying at console
+ * or anywhere at screen the whole value of some sensitive data (like passwords, security tokens, ...),
+ * corresponding string need to be obfuscated a little bit (but remains readable though).
+ * It's not recommended practice, but it could very be helpful either for debug-needs
+ * or when you are working on some kind of <i>Forget Password</i> functionality.
+ */
+public class ZeroSpaceHelper {
+
+    final static String ZERO_SPACE_HTML_ENTITY__LITERAL = "&ZeroWidthSpace;";
+    final static String ZERO_SPACE_HTML_ENTITY__DECIMAL = "&#8203;";
+    final static String ZERO_SPACE_HTML_ENTITY__HEX = "&#x200B;";
+
+    final String zeroSpaceStr;
+
+    public ZeroSpaceHelper() {
+        this(ZERO_SPACE_HTML_ENTITY__DECIMAL);
+    }
+
+    private ZeroSpaceHelper(String zeroSpaceStr) {
+        this.zeroSpaceStr = zeroSpaceStr;
+    }
+
+    ZeroSpaceHelper useLiteral() {
+        return new ZeroSpaceHelper(ZERO_SPACE_HTML_ENTITY__LITERAL);
+    }
+
+    ZeroSpaceHelper useHex() {
+        return new ZeroSpaceHelper(ZERO_SPACE_HTML_ENTITY__HEX);
+    }
+
+    /**
+     * Insert one Zero-Space input passed {@code inputString} at position {@code pos}.
+     *
+     * @param inputString the string to insert zero-space into
+     * @param pos the position to insert from the beginning if positive or from the end if negative
+     * @return obfuscated string (or unchanged if {@code inputString} is blank or {@code pos} is out of range)
+     */
+    public CharSequence insertInto(CharSequence inputString, int pos) {
+        if (StringUtils.isBlank(inputString)) {
+            return inputString;
+        }
+        final int len = inputString.length();
+        if (pos < 0) {
+            pos = len + pos;
+        }
+        if (pos <= 0 || pos >= len) {
+            return inputString;
+        }
+        return inputString.subSequence(0, pos) + zeroSpaceStr + inputString.subSequence(pos, len);
+    }
+
+    /**
+     * Apply {@link #insertInto(CharSequence, int)} two times from beginning and from the end
+     *
+     * @param inputString the string to insert two zero-spaces into
+     * @param pos the position to insert from the beginning and from the end of {@code inputString}
+     * @return obfuscated string (or unchanged if {@code inputString} is blank or {@code pos} is out of range)
+     */
+    public CharSequence insertPair(CharSequence inputString, int pos) {
+        if (pos < 0) {
+            pos = -pos;
+        }
+        int len = StringUtils.isBlank(inputString) ? 0 : inputString.length();
+        CharSequence resultString = insertInto(inputString, pos);
+        if (len - pos > pos) {
+            resultString = insertInto(resultString, -pos);
+        }
+        return resultString;
+    }
+
+    /**
+     * Apply {@link #insertPair(CharSequence, int)} with position {@code 4}
+     *
+     * @param inputString the string to insert two zero-spaces into
+     * @return obfuscated string (or unchanged if {@code inputString} is blank or its length is less than {@code 5})
+     */
+    public CharSequence mask4(CharSequence inputString) {
+        return insertPair(inputString, 4);
+    }
+}
---
+++ src/test/java/org/krmdemo/techlabs/thtool/ZeroSpaceHelperTest.java
@@ -0,0 +1,89 @@
+package org.krmdemo.techlabs.thtool;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * A unit-test for {@link ZeroSpaceHelper}
+ */
+public class ZeroSpaceHelperTest {
+
+    ZeroSpaceHelper zeroSpace = new ZeroSpaceHelper();
+
+    @Test
+    void testBlank() {
+        assertThat(zeroSpace.mask4(null)).isNull();
+        assertThat(zeroSpace.mask4("")).isEmpty();
+        assertThat(zeroSpace.mask4("  \t\n ")).isEqualTo("  \t\n ");
+    }
+
+    @Test
+    void testRangeViolation() {
+        String str10 = "1234567890";
+        assertThat(zeroSpace.insertInto(str10, 0)).isEqualTo(str10);
+        assertThat(zeroSpace.insertInto(str10, -10)).isEqualTo(str10);
+        assertThat(zeroSpace.insertInto(str10, -11)).isEqualTo(str10);
+        assertThat(zeroSpace.insertInto(str10, -111)).isEqualTo(str10);
+        assertThat(zeroSpace.insertInto(str10, 10)).isEqualTo(str10);
+        assertThat(zeroSpace.insertInto(str10, 11)).isEqualTo(str10);
+        assertThat(zeroSpace.insertInto(str10, 111)).isEqualTo(str10);
+    }
+
+    @Test
+    void testDefault() {
+        assertThat(zeroSpace.mask4("1")).isEqualTo("1");
+        assertThat(zeroSpace.mask4("12")).isEqualTo("12");
+        assertThat(zeroSpace.mask4("123")).isEqualTo("123");
+        assertThat(zeroSpace.mask4("1234")).isEqualTo("1234");
+
+        assertThat(zeroSpace.mask4("12345")).isEqualTo("1234&#8203;5");
+        assertThat(zeroSpace.mask4("123456")).isEqualTo("1234&#8203;56");
+        assertThat(zeroSpace.mask4("1234567")).isEqualTo("1234&#8203;567");
+        assertThat(zeroSpace.mask4("12345678")).isEqualTo("1234&#8203;5678");
+
+        assertThat(zeroSpace.mask4("123456789")).isEqualTo("1234&#8203;5&#8203;6789");
+        assertThat(zeroSpace.mask4("1234567890")).isEqualTo("1234&#8203;56&#8203;7890");
+
+        assertThat(zeroSpace.mask4("package org.krmdemo.techlabs.thtool;"))
+            .isEqualTo("pack&#8203;age org.krmdemo.techlabs.tht&#8203;ool;");
+    }
+
+    @Test
+    void testUseLiteral() {
+        assertThat(zeroSpace.useLiteral().mask4("1")).isEqualTo("1");
+        assertThat(zeroSpace.useLiteral().mask4("12")).isEqualTo("12");
+        assertThat(zeroSpace.useLiteral().mask4("123")).isEqualTo("123");
+        assertThat(zeroSpace.useLiteral().mask4("1234")).isEqualTo("1234");
+
+        assertThat(zeroSpace.useLiteral().mask4("12345")).isEqualTo("1234&ZeroWidthSpace;5");
+        assertThat(zeroSpace.useLiteral().mask4("123456")).isEqualTo("1234&ZeroWidthSpace;56");
+        assertThat(zeroSpace.useLiteral().mask4("1234567")).isEqualTo("1234&ZeroWidthSpace;567");
+        assertThat(zeroSpace.useLiteral().mask4("12345678")).isEqualTo("1234&ZeroWidthSpace;5678");
+
+        assertThat(zeroSpace.useLiteral().mask4("123456789")).isEqualTo("1234&ZeroWidthSpace;5&ZeroWidthSpace;6789");
+        assertThat(zeroSpace.useLiteral().mask4("1234567890")).isEqualTo("1234&ZeroWidthSpace;56&ZeroWidthSpace;7890");
+
+        assertThat(zeroSpace.useLiteral().mask4("package org.krmdemo.techlabs.thtool;"))
+            .isEqualTo("pack&ZeroWidthSpace;age org.krmdemo.techlabs.tht&ZeroWidthSpace;ool;");
+    }
+
+    @Test
+    void testUseHex() {
+        assertThat(zeroSpace.useHex().mask4("1")).isEqualTo("1");
+        assertThat(zeroSpace.useHex().mask4("12")).isEqualTo("12");
+        assertThat(zeroSpace.useHex().mask4("123")).isEqualTo("123");
+        assertThat(zeroSpace.useHex().mask4("1234")).isEqualTo("1234");
+
+        assertThat(zeroSpace.useHex().mask4("12345")).isEqualTo("1234&#x200B;5");
+        assertThat(zeroSpace.useHex().mask4("123456")).isEqualTo("1234&#x200B;56");
+        assertThat(zeroSpace.useHex().mask4("1234567")).isEqualTo("1234&#x200B;567");
+        assertThat(zeroSpace.useHex().mask4("12345678")).isEqualTo("1234&#x200B;5678");
+
+        assertThat(zeroSpace.useHex().mask4("123456789")).isEqualTo("1234&#x200B;5&#x200B;6789");
+        assertThat(zeroSpace.useHex().mask4("1234567890")).isEqualTo("1234&#x200B;56&#x200B;7890");
+
+        assertThat(zeroSpace.useHex().mask4("package org.krmdemo.techlabs.thtool;"))
+            .isEqualTo("pack&#x200B;age org.krmdemo.techlabs.tht&#x200B;ool;");
+    }
+}
