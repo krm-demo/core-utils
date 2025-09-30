145f6af8d04b1780c2fd7a8926bdb0d7ebab1111
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2025-09-29 23:42:48 -0400

    Introduce (split the original) 3 versions of 'Usage-XXX.md.th' #2

--- .github/th-tool{force.ansi}.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# -------------------------------------------------------------------------------------------------------
-#   Execute (not build!) 'th-tool' and delegate all command-line of this bash-script's arguments to it.
-# -------------------------------------------------------------------------------------------------------
-# TODO: delete this useless script !!!
-
-mvn --quiet test-compile exec:java@th-tool{force.ansi} -Dexec.args="$*"
