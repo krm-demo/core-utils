46505311530559b88919d8e76ab0c5645fc72bb6
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2025-10-01 10:51:02 -0400

    introduce block/inline fragments tests #1

---
+++ src/test/java/org/krmdemo/techlabs/thymeleaf/ThymeleafConditionTest.java
@@ -0,0 +1,93 @@
+package org.krmdemo.techlabs.thymeleaf;
+
+import lombok.Getter;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.krmdemo.techlabs.core.utils.SysDumpUtils;
+import org.krmdemo.techlabs.thtool.MavenHelper;
+import org.krmdemo.techlabs.thtool.ZeroSpaceHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.krmdemo.techlabs.core.utils.PropertiesUtils.propsMapResource;
+
+/**
+ * This unit-test is just a sandbox for using Thymeleaf-Engine for conditions
+ */
+@Getter  // <-- this one is necessary fpr Thymeleaf-template to access properties via getters
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class ThymeleafConditionTest {
+
+    boolean renderingFragmentA = true;
+    boolean renderingFragmentB = false;
+
+    @Test
+    void testConditionValues() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        Context ctx = new Context(Locale.getDefault());
+        ctx.setVariable("this", this);
+        String renderingProps = templateEngine.process("""
+            renderingFragmentA = [[${this.renderingFragmentA}]];
+            renderingFragmentB = [[${this.renderingFragmentB}]];
+            """, ctx);
+        //System.out.printf("renderingProps:%n---- ---- ---- ----%n%s---- ---- ---- ----%n", renderingProps);
+        assertThat(renderingProps).isEqualTo("""
+            renderingFragmentA = true;
+            renderingFragmentB = false;
+            """);
+    }
+
+    @Test
+    void testConditionalContent_OnlyA() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        Context ctx = new Context(Locale.getDefault());
+        ctx.setVariable("this", this);
+        String conditionalContentA = templateEngine.process("""
+            <th:block th:if="${this.renderingFragmentA}">Something related to 'A' is here !!!</th:block>
+            <th:block th:if="${this.renderingFragmentB}">Something related to 'B' is here !!!</th:block>
+            """, ctx);
+        //System.out.printf("conditionalContentA:%n---- ---- ---- ----%n%s---- ---- ---- ----%n", conditionalContentA);
+        assertThat(conditionalContentA).isEqualTo("""
+            Something related to 'A' is here !!!
+            
+            """);
+    }
+
+    @Test
+    void testConditionalContent_OnlyB() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        Context ctx = new Context(Locale.getDefault());
+        ctx.setVariable("this", this);
+        this.renderingFragmentA = false;
+        this.renderingFragmentB = true;
+        String conditionalContentB = templateEngine.process("""
+            <th:block th:if="${this.renderingFragmentA}">Something related to 'A' is here !!!</th:block>
+            <th:block th:if="${this.renderingFragmentB}">Something related to 'B' is here !!!</th:block>
+            """, ctx);
+        //System.out.printf("conditionalContentB:%n---- ---- ---- ----%n%s---- ---- ---- ----%n", conditionalContentB);
+        assertThat(conditionalContentB).isEqualTo("""
+            
+            Something related to 'B' is here !!!
+            """);
+    }
+
+    @Test
+    void testConditionalContent_UsingUnless() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        Context ctx = new Context(Locale.getDefault());
+        ctx.setVariable("this", this);
+        String conditionalContent_UsingUnless = templateEngine.process("""
+            <th:block th:unless="${this.renderingFragmentA}">Something related to '!A' is here !!!</th:block>
+            <th:block th:unless="${this.renderingFragmentB}">Something related to '!B' is here !!!</th:block>
+            """, ctx);
+        //System.out.printf("conditionalContent_UsingUnless:%n---- ---- ---- ----%n%s---- ---- ---- ----%n", conditionalContent_UsingUnless);
+        assertThat(conditionalContent_UsingUnless).isEqualTo("""
+            
+            Something related to '!B' is here !!!
+            """);
+    }
+}
--- src/test/java/org/krmdemo/techlabs/thymeleaf/ThymeleafFragmentTest.java
+++ src/test/java/org/krmdemo/techlabs/thymeleaf/ThymeleafFragmentTest.java
@@ -26,78 +26,37 @@ public class ThymeleafFragmentTest {
     boolean renderingFragmentB = false;
 
     @Test
-    void testConditionValues() {
+    void testBlock_FragmentA() {
         TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
         Context ctx = new Context(Locale.getDefault());
         ctx.setVariable("this", this);
-        String renderingProps = templateEngine.process("""
-            renderingFragmentA = [[${this.renderingFragmentA}]];
-            renderingFragmentB = [[${this.renderingFragmentB}]];
-            """, ctx);
-        //System.out.printf("renderingProps:%n---- ---- ---- ----%n%s---- ---- ---- ----%n", renderingProps);
-        assertThat(renderingProps).isEqualTo("""
-            renderingFragmentA = true;
-            renderingFragmentB = false;
-            """);
+        String layoutNoFrags = templateEngine.process(".github/th-test-block/test-block-fragment-A.md.th", ctx);
+        //System.out.printf("simpleFragments:%n---- ---- ---- ----%n%s---- ---- ---- ----%n", layoutNoFrags);
+        assertThat(layoutNoFrags).isEqualTo("""
+            This is the **fragment-`A`**: 2 + 3 = 5""");
     }
 
     @Test
-    void testConditionalContent_OnlyA() {
+    void testBlock_FragmentB() {
+        this.someIntProp = 345;
         TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
         Context ctx = new Context(Locale.getDefault());
         ctx.setVariable("this", this);
-        String conditionalContentA = templateEngine.process("""
-            <th:block th:if="${this.renderingFragmentA}">Something related to 'A' is here !!!</th:block>
-            <th:block th:if="${this.renderingFragmentB}">Something related to 'B' is here !!!</th:block>
-            """, ctx);
-        //System.out.printf("conditionalContentA:%n---- ---- ---- ----%n%s---- ---- ---- ----%n", conditionalContentA);
-        assertThat(conditionalContentA).isEqualTo("""
-            Something related to 'A' is here !!!
-            
-            """);
+        String layoutNoFrags = templateEngine.process(".github/th-test-block/test-block-fragment-B.md.th", ctx);
+        //System.out.printf("simpleFragments:%n---- ---- ---- ----%n%s---- ---- ---- ----%n", layoutNoFrags);
+        assertThat(layoutNoFrags).isEqualTo("""
+            This is the **fragment-`B`**: 3 * someIntProp = 1035""");
     }
 
     @Test
-    void testConditionalContent_OnlyB() {
+    void testBlockLayout_NoFragments() {
         TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
         Context ctx = new Context(Locale.getDefault());
         ctx.setVariable("this", this);
-        this.renderingFragmentA = false;
-        this.renderingFragmentB = true;
-        String conditionalContentB = templateEngine.process("""
-            <th:block th:if="${this.renderingFragmentA}">Something related to 'A' is here !!!</th:block>
-            <th:block th:if="${this.renderingFragmentB}">Something related to 'B' is here !!!</th:block>
-            """, ctx);
-        //System.out.printf("conditionalContentB:%n---- ---- ---- ----%n%s---- ---- ---- ----%n", conditionalContentB);
-        assertThat(conditionalContentB).isEqualTo("""
-            
-            Something related to 'B' is here !!!
-            """);
-    }
-
-    @Test
-    void testConditionalContent_UsingUnless() {
-        TemplateEngine templateEngine = new TemplateEngine();
-        Context ctx = new Context(Locale.getDefault());
-        ctx.setVariable("this", this);
-        String conditionalContent_UsingUnless = templateEngine.process("""
-            <th:block th:unless="${this.renderingFragmentA}">Something related to '!A' is here !!!</th:block>
-            <th:block th:unless="${this.renderingFragmentB}">Something related to '!B' is here !!!</th:block>
-            """, ctx);
-        //System.out.printf("conditionalContent_UsingUnless:%n---- ---- ---- ----%n%s---- ---- ---- ----%n", conditionalContent_UsingUnless);
-        assertThat(conditionalContent_UsingUnless).isEqualTo("""
-            
-            Something related to '!B' is here !!!
-            """);
-    }
-
-    @Test
-    void testLayout_NoFragments() {
-        TemplateEngine templateEngine = new TemplateEngine();
-        templateEngine.setTemplateResolver(templateResolverMD());
-        Context ctx = new Context(Locale.getDefault());
-        ctx.setVariable("this", this);
-        String layoutNoFrags = templateEngine.process(".github/th-templates/test-layout-no-frags.md.th", ctx);
+        String layoutNoFrags = templateEngine.process(".github/th-test-block/test-block-layout-no-frags.md.th", ctx);
         //System.out.printf("simpleFragments:%n---- ---- ---- ----%n%s---- ---- ---- ----%n", layoutNoFrags);
         assertThat(layoutNoFrags).isEqualTo("""
             There are no fragments in this layout, but the properties are still processed:
@@ -108,12 +67,12 @@ public class ThymeleafFragmentTest {
     }
 
     @Test
-    void testLayout_SimpleFragments() {
+    void testBlockLayout_SimpleFragments() {
         TemplateEngine templateEngine = new TemplateEngine();
-        templateEngine.setTemplateResolver(templateResolverMD());
+        templateEngine.setTemplateResolver(templateResolver());
         Context ctx = new Context(Locale.getDefault());
         ctx.setVariable("this", this);
-        String layoutNoFrags = templateEngine.process(".github/th-templates/test-layout-two-frags.md.th", ctx);
+        String layoutNoFrags = templateEngine.process(".github/th-test-block/test-block-layout-two-frags.md.th", ctx);
         //System.out.printf("simpleFragments:%n---- ---- ---- ----%n%s---- ---- ---- ----%n", layoutNoFrags);
         assertThat(layoutNoFrags).isEqualTo("""
             This layout contains two fragments ( someIntProp = 1234 ):
@@ -122,13 +81,14 @@ public class ThymeleafFragmentTest {
             """);
     }
 
-    private static ITemplateResolver templateResolverMD() {
+    private static ITemplateResolver templateResolver() {
         // 1. Create a FileTemplateResolver instance
-        FileTemplateResolver templateResolver = new FileTemplateResolver();
-        templateResolver.setTemplateMode("HTML5");
-        templateResolver.setCharacterEncoding("UTF-8");
-        templateResolver.setCacheable(false); // Disable caching for development, enable for production
-        templateResolver.setOrder(1);
-        return templateResolver;
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setOrder(1);
+        resolver.setTemplateMode("HTML5");
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setCacheable(false); // <-- disable caching for development, enable for production
+        resolver.setCheckExistence(true);  // <-- very important for the chain of resolvers
+        return resolver;
     }
 }
