80200195b205fda2f040869642cdb1027c58e66b
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2025-10-30 06:45:34 -0400

    minor corrections in README.md

--- .github/mvn-test--README.md.sh
+++ .github/mvn-test--README.md.sh
@@ -3,6 +3,11 @@
 #   Run the test, which executes "th-tool" template './github/th-templates/ROOT-Readme.md.th'
 # -------------------------------------------------------------------------------------------------------
 
+# deleting the logs
+rm -rf target/logs/th-tool.log
+
 # Note! For maven-failsafe-plugin the system-property should be "it.test", but not just "test" !!!
 mvn clean package "-DskipTests" && mvn failsafe:integration-test "-Dit.test=ThymeleafToolTest#testProcess_Readme"
 
+# see the output in 'th-tool.log'-file
+cat target/logs/th-tool.log
