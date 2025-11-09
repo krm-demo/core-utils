663d19054bd7c3a8ab83f6e02b8b0ff7accbb7af
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2025-11-09 01:09:51 -0500

    introduce new GitHub secrets and vars and bash-script '.github/gpg/gpg-import.sh' #1

--- .github/gpg/gpg-import.sh
+++ .github/gpg/gpg-import.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 # --------------------------------------------------------------------------------------------------------------
-#        Import the GPG-keys from GitHub-secrets (could not be executed, but only copy/pasted into workflow):
+#     Import the GPG-keys from GitHub-secrets (should be executed from GitHub-workflow):
+# - env-var '$GPG_PASSPHRASE' (a GitHub-secret '${{ secrets.GPG_PASSPHRASE }}'),
+#   which must contain GOG-passphrase;
+# - env-var '$GPG_SIGNING_KEY' (a GitHub-secret '${{ secrets.GPG_SIGNING_KEY }}),
+#   which must contain a result of `...>gpg --export-secret-keys 0436EE146A372544 | base64`,
+#   where '0436EE146A372544' is the value of env-var '$GPG_KEY_ID' (a GitHUb-repo-var '${{ vars.GPG_KEY_ID }}');
 # --------------------------------------------------------------------------------------------------------------
 echo "... starting the script $0 in '$(pwd)' ..."
+echo "- GPG_KEY_ID = '$GPG_KEY_ID'"
 
-# save a passphrase in a text-file:
-echo "1qaz@WSX0okm(IJN" > passphrase.txt
+echo "- saving GPG-passphrase to './passphrase.txt' ..."
+echo $GPG_PASSPHRASE > passphrase.txt
 
-# Import GPG-keys from GitHub-secrets
-echo -n "${{ secrets.GPG_SIGNING_KEY }}" | base64 --decode | gpg --batch --import --passphrase-file passphrase.txt
+echo "-------  gpg --import ...  -----------------"
+echo $GPG_SIGNING_KEY | base64 --decode | gpg --batch --import --passphrase-file passphrase.txt
+
+echo "-------  gpg --edit-key ...  ---------------"
+echo -e "5\ny\nquit" | gpg --batch --yes --pinentry-mode loopback --command-fd 0 --expert --edit-key 0436EE146A372544 trust
+
+echo "-------  gpg --update-trustdb ...  ---------"
+gpg --update-trustdb
+
+echo ".... finish the script $0 in '$(pwd)' ...."
