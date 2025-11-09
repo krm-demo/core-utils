dd9c963f0a6f5fb8dc78bbbd5fcca5093ff08076
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2025-11-09 01:52:20 -0500

    introduce new GitHub secrets and vars and bash-script '.github/gpg/gpg-import.sh' #3

--- .github/gpg/gpg-import.sh
+++ .github/gpg/gpg-import.sh
@@ -17,7 +17,7 @@ echo "-------  gpg --import ...  -----------------"
 echo -n "$GPG_SIGNING_KEY" | base64 --decode | gpg --batch --import --passphrase-file passphrase.txt
 
 echo "-------  gpg --edit-key ...  ---------------"
-echo -e "5\ny\nquit" | gpg --batch --yes --pinentry-mode loopback --command-fd 0 --expert --edit-key 0436EE146A372544 trust
+echo -e "5\ny\nquit" | gpg --batch --yes --pinentry-mode loopback --command-fd 0 --expert --edit-key $GPG_KEY_ID trust
 
 echo "-------  gpg --update-trustdb ...  ---------"
 gpg --update-trustdb
