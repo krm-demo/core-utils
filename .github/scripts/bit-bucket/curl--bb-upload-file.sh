affb81da5f216ab72b971a8f7192609285575a39
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2025-11-21 23:36:16 -0500

    a bash-script to curl for list of bb-repos and upload

---
+++ .github/scripts/bit-bucket/curl--bb-upload-file.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# -----------------------------------------------------------------------------------------------------------
+#   A sample script to work with Bit-Bucket-API (loading the list repositories in a workspace)
+# -----------------------------------------------------------------------------------------------------------
+#echo "... starting the script $0 in '$(pwd)' ..."
+
+BB_WORKSPACE="aleksey-kurmanov"
+BB_REPO_NAME="students-db"
+
+# TODO: put it into GitHub-secrets
+MY_BB_TOKEN=$(echo "6F240CE2=8izNQBZl4e2h0b-C6JmEsV2eQGZlLDKm9IVR25rp3jBQXEPmMJM95jH19DV8P24ThX0UKbPacFlrk6CLGVNkFddUEe4HBD9-pOfqw9-vOH1rvovxCGZnaB8hSpvXnbqBdbb2ft2MsudTKUFtHlx3AWJE2fa6lQ9z2hTtXhtIjHr0NGfFx3TTCTA" | rev)
+
+curl -s --request POST \
+  --url "https://api.bitbucket.org/2.0/repositories/${BB_WORKSPACE}/${BB_REPO_NAME}/downloads" \
+  --header "Authorization: Bearer $MY_BB_TOKEN" \
+  -F files=@libs.zip
